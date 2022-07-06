be73be4a695713e8ad2672ddd1fe0b723c558e86
月明かりのジェロニモ <86349647+Geronimo-moon@users.noreply.github.com>
2022-07-06 11:55:39 +0900

    redirect changed

--- server.go
+++ server.go
@@ -3,29 +3,10 @@ package main
 import (
 	"net/http"
 	"os"
-	"math/rand"
-	"time"
 )
 
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	redirects := []string{"/react","/vanilla"}
-
-	rand.Seed(time.Now().UnixNano())
-
-	http.Redirect(w, r, redirects[rand.Intn(len(redirects))], 301)
-}
-
 func main() {
-	http.HandleFunc("/", redirectHandler)
-
-	imgFs := http.FileServer(http.Dir("static/img"))
-	http.Handle("/img", imgFs)
-
-	vanillaFs := http.FileServer(http.Dir("static/vanilla"))
-	http.Handle("/vanilla", vanillaFs)
-
-	reactFs := http.FileServer(http.Dir("static/react"))
-	http.Handle("/react", reactFs)
+	http.HandleFunc("/", "static")
 
 	port := os.Getenv("PORT")
 	if port == "" {
