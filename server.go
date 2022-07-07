06abdc3a7869e5143d4bc5b69edd06fbfab39303
月明かりのジェロニモ <86349647+Geronimo-moon@users.noreply.github.com>
2022-07-07 12:05:47 +0900

    add db-insertion

--- server.go
+++ server.go
@@ -11,20 +11,40 @@ import (
 	"os"
 )
 
-func formWriter(w http.ResponseWriter, r *http.Request) {
+type WISHES struct {
+	ID string 
+	Text string
+}
+
+func formGetter(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("wishtext", r.FormValue("wishtext"))
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 			log.Fatalf("Error opening database: %q", err)
 	}
 	defer db.Close()
+}
 
-	if err := db.Ping(); err != nil {
-		log.Fatal("PingError: ", err)
-	}else{
-		fmt.Println("Successed to connect %q", os.Getenv("DATABASE_URL"))
+func formWriter(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+			log.Fatalf("Error opening database: %q", err)
+	}
+	defer db.Close()
+
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS wishes_with_text (
+			id SERIAL NOT NULL PRIMARY KEY,
+			text TEXT NOT NULL
+		);`
+	)
+
+	if err != nil {
+		log.Fatalf("Fatal error has occured: %q", err)
 	}
 
+	_, err := db.Exec("INSERT INTO wishes_with_text (text) VALUES (" + r.FormValue("wishtext") + ");")
+
+	http.Redirect(w, r, "/", 301)
 }
 
 func main() {
@@ -32,6 +52,8 @@ func main() {
 	http.Handle("/", fs)
 
 	http.HandleFunc("/send",formWriter)
+
+	http.HandleFunc("/get",formGetter)
 
 	port := os.Getenv("PORT")
 	if port == "" {
